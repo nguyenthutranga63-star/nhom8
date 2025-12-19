142948d2bb73bdb9c9961b80f0206d3756136329
nguyenthutranga63-star <nguyenthutranga63@gmail.com>
2025-12-19 16:35:29 +0700

    nhom8

---
+++ nhom8/nhom8/src/main/java/com/gmail/nhom8/Nhom8Application.java
@@ -0,0 +1,13 @@
+package com.gmail.nhom8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nhom8Application.class, args);
+	}
+
+}
---
+++ nhom8/nhom8/src/test/java/com/gmail/nhom8/Nhom8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.nhom8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
